df5c17fa20417e5daef5e8b9b35353458c37547b
prabal-007 <gprabal123@gmail.com>
2022-03-12 23:24:10 +0530

    seting polyson environment

--- elonNFT/hardhat.config.js
+++ elonNFT/hardhat.config.js
@@ -1,21 +1,19 @@
-require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-  solidity: "0.8.4",
-};
+ require('dotenv').config(); //all the key value pairs are being made available due to this lib
+ require('@nomiclabs/hardhat-ethers');
+  
+ const {POLYGON_API_URL_KEY, PRIVATE_KEY} = process.env; //environment variables are being loaded here.
+  
+ module.exports = {
+   solidity: "0.8.1",
+   defaultNetwork: 'rinkeby',
+   networks: {
+     hardhat: {},
+     matic: {
+         url: POLYGON_API_URL_KEY,
+         accounts: [`0x${PRIVATE_KEY}`]
+     },
+   },
+ };
